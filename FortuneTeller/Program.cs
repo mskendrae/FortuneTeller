a4d2f705de4c2826322fbfcaff95b0982c48ed38
Kendra Egert <mskendrae@gmail.com>
2016-09-09 18:51:16 -0400

    first commit

---
+++ FortuneTeller/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to my Fortune Teller App.  With a little bit of information from you in will be able to tell your future.");
+
+            Console.WriteLine("Please enter your First Name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your Last Name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age:");
+            string userAge = Console.ReadLine();
+
+            Console.WriteLine("Please enter the month you were born 1 to 12.");
+            int bMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your favorite ROYGBIV color. Type \"Help\" to get a list of the ROYGBIV Colors.");
+            string royGbiv = Console.ReadLine());
+
+               if (royGbiv.ToLower() == "help")
+                     {
+
+                     }
+
+
+            
+
+        }
+    }
+}
