7e57a3faece96bad82ae544fc73f4ac41cbfd30d
Kendra Egert <Kendra Egert>
2016-09-11 11:00:10 -0400

    added sibling code

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -62,72 +62,103 @@ namespace FortuneTeller
                           savings = 0;
 
                       } 
+
+
             Console.WriteLine("Please enter your favorite ROYGBIV color. Type \"Help\" to get a list of the ROYGBIV Colors.");
             string royGbiv = Console.ReadLine();
             string vehicle = "yellow sub";
 
-            if (royGbiv.ToLower() == "help")
-            {
-                Console.WriteLine("The ROYGBIV are as follows. Please type the name of your favorite color not the number.");
-                Console.WriteLine("1. Red");
-                Console.WriteLine("2. Orange");
-                Console.WriteLine("3. Yellow");
-                Console.WriteLine("4. Green");
-                Console.WriteLine("5. Blue");
-                Console.WriteLine("6. Indigo");
-                Console.WriteLine("7. Violet");
-                Console.WriteLine("Please enter color name and not the number.");
-                royGbiv = Console.ReadLine();
+                if (royGbiv.ToLower() == "help")
+                     {
+                        Console.WriteLine("The ROYGBIV are as follows. Please type the name of your favorite color not the number.");
+                        Console.WriteLine("1. Red");
+                        Console.WriteLine("2. Orange");
+                        Console.WriteLine("3. Yellow");
+                        Console.WriteLine("4. Green");
+                        Console.WriteLine("5. Blue");
+                        Console.WriteLine("6. Indigo");
+                        Console.WriteLine("7. Violet");
+                        Console.WriteLine("Please enter color name and not the number.");
+                                royGbiv = Console.ReadLine();
 
-            }
+                      }
 
-            if (royGbiv.ToLower() == "red")
-            {
-                vehicle = "yaht";
-            }
+                if (royGbiv.ToLower() == "red")
+                     {
+                       vehicle = "yaht";
+                     }
 
-            else if (royGbiv.ToLower() == "Orange")
-            {
-                 vehicle = "classic Mustang";
+                else if (royGbiv.ToLower() == "Orange")
+                     {
+                       vehicle = "classic Mustang";
 
-            }
+                     }
 
-            else if (royGbiv.ToLower() == "yellow")
-            {
+                else if (royGbiv.ToLower() == "yellow")
+                     {
+                       vehicle = "private Jet";
 
-                 vehicle = "private Jet";
+                     }
 
-            }
+                else if (royGbiv.ToLower() == "green")
+                     {
+                       vehicle = "Harley Davidson";
 
-            else if (royGbiv.ToLower() == "green")
-            {
+                     }
 
-                 vehicle = "Harley Davidson";
+                else if (royGbiv.ToLower() == "blue")
+                     {
+                        vehicle = "glider";
 
-            }
+                     }
 
-            else if (royGbiv.ToLower() == "blue")
-            {
+                else if (royGbiv.ToLower() == "indigo")
+                      {
+                       vehicle = "Shwinn bicycle";
 
-                vehicle = "glider";
+                      }
 
-            }
-            else if (royGbiv.ToLower() == "indigo")
-            {
+                else if (royGbiv.ToLower() == "violet")
+                      {
+                        vehicle = "sea plane";
 
-                vehicle = "Shwinn bicycle";
-            }
-
-            else if (royGbiv.ToLower() == "violet")
-            {
-                vehicle = "sea plane";
-
-            }
+                      }
 
 
-                        
-          
+            Console.WriteLine("Please enter the number of siblings you have");
+            int siblings = int.Parse(Console.ReadLine());
+            string vacationHome = "outhouse";
 
-        }
+                if (siblings == 0)
+                   {
+
+                      vacationHome = "Mansion";
+                    }
+                 
+                else if (siblings == 1)
+                    {
+
+                       vacationHome = "Townhouse";
+                    }
+
+                else if (siblings == 2)
+                    {
+
+                        vacationHome == "House Boat";
+                    }
+
+                 else if (siblings == 3)
+                   {
+
+                        vacationHome == "Starship Enterprise"
+                   }
+
+                 else (siblings > 3)
+                  {
+                        vacationHome == "Double Wide Trailer"
+                   }
+        }        
+
+
     }
 }
