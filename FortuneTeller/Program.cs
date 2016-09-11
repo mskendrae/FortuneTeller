01f8434575c2d048b3175895a15310495794aa9c
Kendra Egert <Kendra Egert>
2016-09-11 11:17:29 -0400

    added final writeline

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -23,142 +23,145 @@ namespace FortuneTeller
             int userAge = int.Parse(Console.ReadLine());
             int retire = 0;
 
-                if (userAge % 2 == 0 )
-                   {
-                    int evenAge = userAge;
+            if (userAge % 2 == 0)
+            {
+                int evenAge = userAge;
 
-                     retire = 10;
-                    }
-                else
-                    {
-                      int oddAge = userAge;
+                retire = 10;
+            }
+            else
+            {
+                int oddAge = userAge;
 
-                      retire = 20; 
+                retire = 20;
 
-                    }
+            }
 
 
             Console.WriteLine("Please enter the month you were born 1 to 12 no leading zeros please");
-              int bMonth = int.Parse(Console.ReadLine());
-              int savings = 0
+            int bMonth = int.Parse(Console.ReadLine());
+            int savings = 0
                 if (bMonth >= 1 && bMonth <= 4)
-                     {
-                          savings = 100000;
+            {
+                savings = 100000;
 
-                     }
-                else if (bMonth >=5 && bMonth <= 8)
-                    {
+            }
+            else if (bMonth >= 5 && bMonth <= 8)
+            {
 
-                        savings = 200000;
-                    }
+                savings = 200000;
+            }
 
-                else if (bMonth >=9 && bMonth <=12)
-                     {
+            else if (bMonth >= 9 && bMonth <= 12)
+            {
 
-                        savings = 500000;
-                      }
-                else
-                      {
-                          savings = 0;
+                savings = 500000;
+            }
+            else
+            {
+                savings = 0;
 
-                      } 
+            }
 
 
             Console.WriteLine("Please enter your favorite ROYGBIV color. Type \"Help\" to get a list of the ROYGBIV Colors.");
             string royGbiv = Console.ReadLine();
             string vehicle = "yellow sub";
 
-                if (royGbiv.ToLower() == "help")
-                     {
-                        Console.WriteLine("The ROYGBIV are as follows. Please type the name of your favorite color not the number.");
-                        Console.WriteLine("1. Red");
-                        Console.WriteLine("2. Orange");
-                        Console.WriteLine("3. Yellow");
-                        Console.WriteLine("4. Green");
-                        Console.WriteLine("5. Blue");
-                        Console.WriteLine("6. Indigo");
-                        Console.WriteLine("7. Violet");
-                        Console.WriteLine("Please enter color name and not the number.");
-                                royGbiv = Console.ReadLine();
+            if (royGbiv.ToLower() == "help")
+            {
+                Console.WriteLine("The ROYGBIV are as follows. Please type the name of your favorite color not the number.");
+                Console.WriteLine("1. Red");
+                Console.WriteLine("2. Orange");
+                Console.WriteLine("3. Yellow");
+                Console.WriteLine("4. Green");
+                Console.WriteLine("5. Blue");
+                Console.WriteLine("6. Indigo");
+                Console.WriteLine("7. Violet");
+                Console.WriteLine("Please enter color name and not the number.");
+                royGbiv = Console.ReadLine();
 
-                      }
+            }
 
-                if (royGbiv.ToLower() == "red")
-                     {
-                       vehicle = "yaht";
-                     }
+            if (royGbiv.ToLower() == "red")
+            {
+                vehicle = "yaht";
+            }
 
-                else if (royGbiv.ToLower() == "Orange")
-                     {
-                       vehicle = "classic Mustang";
+            else if (royGbiv.ToLower() == "Orange")
+            {
+                vehicle = "classic Mustang";
 
-                     }
+            }
 
-                else if (royGbiv.ToLower() == "yellow")
-                     {
-                       vehicle = "private Jet";
+            else if (royGbiv.ToLower() == "yellow")
+            {
+                vehicle = "private Jet";
 
-                     }
+            }
 
-                else if (royGbiv.ToLower() == "green")
-                     {
-                       vehicle = "Harley Davidson";
+            else if (royGbiv.ToLower() == "green")
+            {
+                vehicle = "Harley Davidson";
 
-                     }
+            }
 
-                else if (royGbiv.ToLower() == "blue")
-                     {
-                        vehicle = "glider";
+            else if (royGbiv.ToLower() == "blue")
+            {
+                vehicle = "glider";
 
-                     }
+            }
 
-                else if (royGbiv.ToLower() == "indigo")
-                      {
-                       vehicle = "Shwinn bicycle";
+            else if (royGbiv.ToLower() == "indigo")
+            {
+                vehicle = "Shwinn bicycle";
 
-                      }
+            }
 
-                else if (royGbiv.ToLower() == "violet")
-                      {
-                        vehicle = "sea plane";
+            else if (royGbiv.ToLower() == "violet")
+            {
+                vehicle = "sea plane";
 
-                      }
+            }
 
 
             Console.WriteLine("Please enter the number of siblings you have");
             int siblings = int.Parse(Console.ReadLine());
-            string vacationHome = "outhouse";
+            string vacationLocation = "outhouse";
 
-                if (siblings == 0)
-                   {
+            if (siblings == 0)
+            {
 
-                      vacationHome = "Mansion";
-                    }
-                 
-                else if (siblings == 1)
-                    {
+                vacationLocation = "Italy";
+            }
 
-                       vacationHome = "Townhouse";
-                    }
+            else if (siblings == 1)
+            {
 
-                else if (siblings == 2)
-                    {
+                vacationLocation = "Scotland";
+            }
 
-                        vacationHome == "House Boat";
-                    }
+            else if (siblings == 2)
+            {
 
-                 else if (siblings == 3)
-                   {
+                vacationLocation = "Florida Coast";
+            }
 
-                        vacationHome == "Starship Enterprise"
-                   }
+            else if (siblings == 3)
+            {
 
-                 else (siblings > 3)
-                  {
-                        vacationHome == "Double Wide Trailer"
-                   }
-        }        
+                vacationLocation = "the moon";
+             }
 
+            else  if (siblings > 3)
+             {
+                vacationLocation = "South Dakota";
+              
+             }
+
+            Console.WriteLine(firstName + " " + lastName  + " will retire in " + retire + " with " + savings + " in the bank, a vacation home in " + vacationLocation + " and a " + vehicle);
+
+        }
 
     }
 }
